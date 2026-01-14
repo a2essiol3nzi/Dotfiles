76e9bb8d683c98596ff794a6029b7dccbd5cf70e
Alessio Lenzi <alenzi.cs@gmail.com>
2026-01-14 12:43:39 +0100

    minor changes
    personal modification based on preferences and other programs

--- sway/Scripts/lockscreen_sway.sh
+++ sway/Scripts/lockscreen_sway.sh
@@ -10,7 +10,7 @@ swayidle \
     resume 'swaymsg "output * dpms on"' &
 
 # Locks the screen immediately
-swaylock -c 65737e
+swaylock -c 9b85f2
 
 # Kills last background task so idle timer doesn't keep running
 kill %%
